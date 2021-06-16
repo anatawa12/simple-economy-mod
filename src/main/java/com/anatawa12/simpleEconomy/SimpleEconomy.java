4f22b86ef836f7170e50ed092ffeb2cb7263dc4c
anatawa12 <anatawa12@icloud.com>
2021-06-16 20:13:43 +0900

    add PlayerMoney

--- src/main/java/com/anatawa12/simpleEconomy/SimpleEconomy.java
+++ src/main/java/com/anatawa12/simpleEconomy/SimpleEconomy.java
@@ -1,7 +1,26 @@
 package com.anatawa12.simpleEconomy;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.EntityEvent;
 
 @Mod(modid = "simple-economy-mod")
 public class SimpleEconomy {
+    @Mod.EventHandler
+    public void onPreInit(FMLPreInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onEntityConstructing(EntityEvent.EntityConstructing event) {
+        if (!event.entity.worldObj.isRemote && event.entity instanceof EntityPlayer) {
+            if (!PlayerMoney.identifier.equals(event.entity.registerExtendedProperties(
+                    PlayerMoney.identifier, new PlayerMoney()))) {
+                throw new IllegalStateException("duplicated extended prop name: " + PlayerMoney.identifier);
+            }
+        }
+    }
 }

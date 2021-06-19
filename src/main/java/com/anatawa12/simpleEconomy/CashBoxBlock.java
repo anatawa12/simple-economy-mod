0824b07948311c69644217a007114b9a67d1a3ba
anatawa12 <anatawa12@icloud.com>
2021-06-19 19:26:13 +0900

    add texture

--- src/main/java/com/anatawa12/simpleEconomy/CashBoxBlock.java
+++ src/main/java/com/anatawa12/simpleEconomy/CashBoxBlock.java
@@ -1,14 +1,19 @@
 package com.anatawa12.simpleEconomy;
 
 import com.anatawa12.simpleEconomy.gui.GuiHandler;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.IIcon;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
 
@@ -18,6 +23,9 @@ import static com.anatawa12.simpleEconomy.SimpleEconomy.MONEY_LOGGER;
 
 public final class CashBoxBlock extends BlockContainer {
     public static CashBoxBlock INSTANCE = new CashBoxBlock();
+
+    @SideOnly(Side.CLIENT)
+    private IIcon frontIcon;
 
     @SuppressWarnings("unused")
     private CashBoxBlock() {
@@ -69,6 +77,22 @@ public final class CashBoxBlock extends BlockContainer {
 
     @Override
     public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase placer, ItemStack item) {
+        int direction = MathHelper.floor_double((double)(placer.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+        switch (direction) {
+            case 0:
+                world.setBlockMetadataWithNotify(x, y, z, 2, 2);
+                break;
+            case 1:
+                world.setBlockMetadataWithNotify(x, y, z, 5, 2);
+                break;
+            case 2:
+                world.setBlockMetadataWithNotify(x, y, z, 3, 2);
+                break;
+            case 3:
+                world.setBlockMetadataWithNotify(x, y, z, 4, 2);
+                break;
+        }
+
         if (world.isRemote) return;
         CashBoxTileEntity tileEntity = CashBoxTileEntity.getOrSet(world, x, y, z);
         MONEY_LOGGER.info("placed cash box by {} in dimension #{} at ({}, {}, {})",
@@ -78,6 +102,22 @@ public final class CashBoxBlock extends BlockContainer {
         }
 
         super.onBlockPlacedBy(world, x, y, z, placer, item);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public IIcon getIcon(int side, int meta)
+    {
+        // extra: for item rendering
+        if (meta == 0 && side == 3) return this.frontIcon;
+        if (side != meta) return this.blockIcon;
+        return this.frontIcon;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister p_149651_1_)
+    {
+        this.blockIcon = p_149651_1_.registerIcon("iron_block");
+        this.frontIcon = p_149651_1_.registerIcon("simple-economy-mod:cash_box_front");
     }
 
     @Override
